4a6e5e890126346f1084f11a706ebcc24aedc585
Ian Findlay <code@findlayian.com>
2022-10-01 23:12:38 +0100

    Remove console.log

--- amazon--clone/src/utils/formatCurrency.js
+++ amazon--clone/src/utils/formatCurrency.js
@@ -2,7 +2,6 @@ function formatCurrency(currency) {
   try {
     currency = Number(currency)
   } catch {
-    console.log(currency)
     currency = Number(currency.replace(",", ""))
   }
   return Intl.NumberFormat("en-GB").format(currency.toFixed(2))
